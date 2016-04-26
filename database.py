20bb9bed5b789f89a28c04a68bdf7dbd2a6c0167
Daniel Konovalenko <ddkono@gmail.com>
2016-04-26 18:20:11 +0300

    Added get_friends function to database helper

--- database.py
+++ database.py
@@ -9,4 +9,10 @@ def get_age(user_id):
     return db.user_data.findOne({"id": user_id})["age"]
 
 
-
+def get_friends(user_id):
+    result = db.user_friends.findOne({"id": user_id})
+    if result:
+        return result
+    else:
+        return None
+    
