7e3c15713e061a1d535c8cfde36e4a40d60ed2a4
Daniel Konovalenko <ddkono@gmail.com>
2016-04-26 19:53:56 +0300

    Fixed getters

--- database.py
+++ database.py
@@ -6,13 +6,12 @@ client = MongoClient('localhost', 27017)
 db = client.odnoklassniki
 
 def get_age(user_id):
-    return db.user_data.findOne({"id": user_id})["age"]
+    return db.user_data.find_one({"id": user_id})["age"]
 
 
 def get_friends(user_id):
-    result = db.user_friends.findOne({"id": user_id})
+    result = db.user_friends.find_one({"id": user_id})
     if result:
         return result
     else:
         return None
-    
