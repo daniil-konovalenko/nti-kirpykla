f648b3d0f0515d44cea2b5f9f17224ea5344c8bc
Daniel Konovalenko <ddkono@gmail.com>
2016-04-27 13:14:57 +0300

    Added get_location(user_id)

--- Day_1/database.py
+++ Day_1/database.py
@@ -13,7 +13,8 @@ user_data = db.user_data
 def get_age(user_id):
     return db.user_data.find_one({"_id": user_id})["birth_date"]
 
-
+def get_location(user_id):
+    return db.user_data.find_one({"_id": user_id})["location_id"]
 
 def get_friends(user_id):
     result = db.user_friends.find_one({"_id": user_id})
