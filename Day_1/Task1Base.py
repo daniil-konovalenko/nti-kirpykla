03d3d01678cfce3405ff50555c6f5f7b52fdf358
Kirill Shumilov <shumilov.kirill98@gmail.com>
2016-04-27 10:40:17 +0300

    Reorganized file system

--- Day_1/Task1Base.py
+++ Day_1/Task1Base.py
@@ -1,13 +1,13 @@
-import os
+import pickle
 import random
 import sys
-import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
-from relations import is_relevant, is_probably_same_age
 from sklearn.linear_model import LinearRegression
-import pymongo
+
+from Day_1.relations import is_relevant, is_probably_same_age
+
 
 def visualisation(data: np.ndarray, a, b):
     x_line = [random.randint(0, 100) for x in range(10)]
@@ -16,6 +16,12 @@ def visualisation(data: np.ndarray, a, b):
     plt.scatter(data[:, 0], data[:, 1])
     plt.show()
 
+
+def common_friends(userId_1: int, userId_2: list) -> float:
+    neighborhood_1 = set(map(lambda x: x[0], get_friends(userId_1)))
+    neighborhood_2 = set(map(lambda x: x[0], get_friends(userId_2)))
+    c_friends = list(neighborhood_1 & neighborhood_2)
+    return len(c_friends)
 
 
 def jaccard_coefficient(userId_1: int, userId_2: int) -> float:
@@ -26,56 +32,45 @@ def jaccard_coefficient(userId_1: int, userId_2: int) -> float:
     return len(c_friends) / len(all_friends)
 
 
+def jaccard_from_kailiak(userId_1: int, userId_2: int, graph: dict) -> float:
+    # user1 is user for which we make a prediction
+    neighborhood_1 = set(map(lambda x: x[0], graph[userId_1]))
+    neighborhood_2 = set(map(lambda x: x[0], graph[userId_2]))
+    c_friends = neighborhood_1 & neighborhood_2
+    return len(c_friends) / len(neighborhood_1)
+
+
 def prediction_function():
     results = np.empty((0, 2))
     y = np.empty((0, 1))
     without_age = list()
-    for userId, neighborhood in db.user_friends.find():
+    missed = list()
+    for user in db.user_friends.find():
+        userId = user['id']
+        neighborhood = user['friends']
         try:
             age = get_age(userId)
             if age != None:
                 y = np.vstack((y, age))
         except:
             without_age.append(userId)
+            print(userId)
             continue
+        print('Age for user {} have to be predicted'.format(userId))
         try:
-            print('Age for user {} have to be predicted'.format(userId))
-            neighborhood = list()
-            if len(neighborhood) == 1:
-                neighborhood.append(is_relevant(neighborhood))
-            else:
-                for user in neighborhood:
-                    a = is_relevant(user)
-                    neighborhood.append(a)
-            neighborhood = np.array(neighborhood)
+            neighborhood = np.array(list(filter(is_relevant, neighborhood)))
+            print("{}' neighborhood exists".format(userId))
+            jaccard_score = np.array([jaccard_coefficient(userId, user[0]) for user in neighborhood])
+            print(jaccard_score)
+            probaility_score = np.array(list(map(is_probably_same_age, neighborhood)))
+            ages = np.array(list(map(lambda user: get_age(user[0]), neighborhood)))
+            print(ages)
+            result = np.sum(jaccard_score * probaility_score * ages) / np.sum(jaccard_score)
+            result = np.hstack((userId, result))
+            results = np.vstack((results, result))
         except KeyError:
+            missed.append(userId)
             continue
-        print("{}' neighborhood exists".format(userId))
-        probaility_score = list()
-        jaccard_score = list()
-        ages = list()
-
-        for i, user in enumerate(neighborhood):
-            try:
-                jaccard_score.append(jaccard_coefficient(userId, user[0]))
-            except KeyError:
-                jaccard_score.append(0.5)
-            prob_score = is_probably_same_age(user)
-            probaility_score.append(prob_score)
-            try:
-                ages.append(get_age(user[1]))
-            except KeyError:
-                jaccard_score.pop()
-                probaility_score.pop()
-                continue
-        ages = np.array(ages)
-        print(ages.shape)
-        jaccard_score = np.array(jaccard_score)
-        print(jaccard_score.shape)
-        probaility_score = np.array(probaility_score)
-        result = np.sum(jaccard_score * probaility_score * ages) / np.sum(jaccard_score)
-        result = np.hstack((userId, result))
-        results = np.vstack((results, result))
     return y, results, without_age
 
 
@@ -125,8 +120,16 @@ def bl(graph, demog, fd=False):
             fd.write(str(pId) + '\t' + str(avg) + '\n')
     return res
 
-''''
-from GraphParser import graphParser
+
+from Day_1.GraphParser import graphParser
+
+cols = list()
+cols.append("userId")
+cols.append("birth_date")
+(demog, fd) = graphParser.parseFolderBySchema("Task1/Task1/trainDemography", 0, "", "userId", cols, True)
+
+
+
 cols = list()
 cols.append("from")
 cols.append("to")
@@ -136,15 +139,7 @@ cols.append('mask')
 with open("graph.pkl", "wb") as fout:
     pickle.dump(graph, fout)
 
-cols = list()
-cols.append("userId")
-cols.append("birth_date")
-(demog, fd) = graphParser.parseFolderBySchema("Task1/Task1/trainDemography", 0, "", "userId", cols, True)
-with open("demog.pkl", "wb") as fout:
-    pickle.dump(demog, fout)
-graph = pickle.load(open('graph.pkl', 'rb'))
-'''
-y, results, without_age = prediction_function()
+y, results, without_age = prediction_function(demog, graph)
 X = results[:, 1]
 model = LinearRegression(normalize=True, n_jobs=-1)
 model.fit(X, y)
